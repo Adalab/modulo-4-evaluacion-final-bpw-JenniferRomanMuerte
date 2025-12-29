c2434f452ac8401cf532673fabfdbe735e30f872
Jennifer <jenniferromanmuerte@gmail.com>
2025-12-29 10:05:03 +0100

    Changed endpoints user for song and added endpoint get One Song

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-
-
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const mysql = require('mysql2/promise');
-
+const mysql = require("mysql2/promise");
 
 // Creamos el servidor
 const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({limit: '25mb'}));
+server.use(express.json({ limit: "25mb" }));
 
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 3000;
@@ -23,11 +20,11 @@ server.listen(serverPort, () => {
 // Conexion a la BBDD
 async function getConnection() {
   const connection = await mysql.createConnection({
-    host: '127.0.0.1',
-    database: 'music_manager',
-    user: 'jennifer',
-    password: 'Simba&Nala82',
-    port: 3307
+    host: "127.0.0.1",
+    database: "music_manager",
+    user: "jennifer",
+    password: "Simba&Nala82",
+    port: 3307,
   });
   await connection.connect();
 
@@ -38,13 +35,29 @@ async function getConnection() {
   return connection;
 }
 
-// Escribimos los endpoints que queramos
-server.get('/api/users', async (req, res) => {
+/******************
+ENDPOINTS
+*******************/
 
-  let queryAllUsers = 'SELECT * FROM users';
+// RECUPERAR UN  USUARIO
+server.get("/api/songs/:songId", async (req, res) => {
+  let queryOneSong = "SELECT * FROM songs where id = ?";
 
   const connection = await getConnection();
-  const [results] = await connection.query(queryAllUsers);
+  const [results] = await connection.query(queryOneSong, [req.params.songId]);
+
+  await connection.end();
+  if (results.length === 0) return res.status(404).json({ error: "No existe" });
+  return res.json(results[0]);
+});
+
+// RECUPERAR TODOS LOS USUARIOS
+server.get("/api/songs", async (req, res) => {
+  let queryAllSongs = "SELECT * FROM songs";
+
+  const connection = await getConnection();
+  const [results] = await connection.query(queryAllSongs);
+  await connection.end();
+
   res.json(results);
-  connection.end();
 });
